a02d0c5fa4b6677e13394cdddc87411e727acc35
Eric Horodyski <horodyski@ionic.io>
2020-10-20 17:58:48 -0400

    Initial test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Plugins } from "@capacitor/core";
+import { delay, map } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+
+const { App, BackgroundTask } = Plugins;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  data: Observable<string> = of("");
+  pendingRequest: boolean = false;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.sendMessage();
+    App.addListener("appStateChange", (state) => {
+      if (!state.isActive && this.pendingRequest) {
+        let taskId = BackgroundTask.beforeExit(() => {
+          this.data.subscribe((data) =>
+            console.log(`E/ ${data} - ${new Date(Date.now()).getTime()}`)
+          );
+          BackgroundTask.finish({ taskId });
+        });
+      }
+    });
+  }
+
+  sendMessage() {
+    this.pendingRequest = true;
+    this.data = this.http
+      .get<any>("http://dummy.restapiexample.com/api/v1/employees")
+      .pipe(
+        delay(5000),
+        map((data) => {
+          this.pendingRequest = true;
+          return data.status;
+        })
+      );
+  }
 }
